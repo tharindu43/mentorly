3d33d8c65c486d6c4b6455dcace3bb8e9dd0baa3
tharindu43 <thariduperera09@gmail.com>
2025-04-10 15:41:08 +0530

    Update AchievementPostRequest.java
    change

---
+++ springboot-backend/src/main/java/com/example/mentorly/achievementpost/AchievementPostRequest.java
@@ -0,0 +1,26 @@
+package com.example.mentorly.achievementpost;
+
+import com.example.mentorly.template.TemplateData;
+import com.example.mentorly.template.TemplateType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AchievementPostRequest {
+    @NotBlank(message = "Skill cannot be blank")
+    private String skill;
+
+    @NotBlank(message = "Title cannot be blank")
+    private String title;
+
+    @NotNull(message = "Template type cannot be null")
+    private TemplateType templateType;
+
+    @NotNull(message = "Template data cannot be null")
+    private TemplateData templateData;
+}
