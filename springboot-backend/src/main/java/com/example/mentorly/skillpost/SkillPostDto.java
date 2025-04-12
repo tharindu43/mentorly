baf7a4e391f87799f65c558cbe3384a338522584
akenyaneth <nethmiakashane@gmail.com>
2025-04-12 16:18:12 +0530

    Update SkillPostDto.java
    update

---
+++ springboot-backend/src/main/java/com/example/mentorly/skillpost/SkillPostDto.java
@@ -0,0 +1,30 @@
+package com.example.mentorly.skillpost;
+
+import com.example.mentorly.comment.CommentDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class SkillPostDto {
+    private String skillPostId;
+    private String authorId;
+    private String authorName;
+    private String authorProfileImageUrl;
+    private ArrayList<String> skillPostImageUrls;
+    private String skillPostVideoUrl;
+    private String skillPostVideoThumbnailUrl;
+    private Integer videoDurationSeconds;
+    private String skillName;
+    private String title;
+    private String description;
+    private int noOfLikes;
+    private Set<String> likedUserIds;
+    private List<CommentDto> comments;
+    private LocalDateTime createdAt;
+}
