64990aa2a2af66c5c618596b514991b4d808e24f
akenyaneth <nethmiakashane@gmail.com>
2025-04-14 14:27:41 +0530

    Update UserController.java
    jj

--- springboot-backend/src/main/java/com/example/mentorly/user/UserController.java
+++ springboot-backend/src/main/java/com/example/mentorly/user/UserController.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
+    
 
     @GetMapping("/me")
     public ResponseEntity<UserDto> getCurrentUser(
