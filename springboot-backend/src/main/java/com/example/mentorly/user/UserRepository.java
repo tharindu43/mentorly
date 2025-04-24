99a32ac0fcea19c30c75d399ca12a0bb71a464dc
tharindu43 <thariduperera09@gmail.com>
2025-04-24 17:38:56 +0530

    m
    mm

--- springboot-backend/src/main/java/com/example/mentorly/user/UserRepository.java
+++ springboot-backend/src/main/java/com/example/mentorly/user/UserRepository.java
@@ -13,3 +13,4 @@ public interface UserRepository extends MongoRepository<User, String> {
     List<User> findByEmailContainingIgnoreCaseOrNameContainingIgnoreCaseOrBioContainingIgnoreCase(
             String email, String name, String bio);
 }
+// import org.springframework.stereotype.Service;
